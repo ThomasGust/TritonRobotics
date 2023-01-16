40bb55934e7973e1b603deb41d1249457e9e2439
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-15 22:14:25 -0800

    Update video_capture.py

--- raspberry_pi/information_system/test/video_capture.py
+++ raspberry_pi/information_system/test/video_capture.py
@@ -4,13 +4,18 @@ print(np.__version__)
 import cv2
 print(cv2.__version__)
 
+
 cam = cv2.VideoCapture(0)
 
-result, img = cam.read()
+def capture(name):
+    result, img = cam.read()
 
-if result:
-    cv2.imwrite("/home/tritonrobotics2/Desktop/TESTIMAGE1.jpg", img)
-    print("saved image")
+    if result:
+        cv2.imwrite(f"/home/tritonrobotics2/Desktop/{name}.jpg", img)
+        print("saved image")
 
-else:
-    print("NO RESULT")
+    else:
+        print("NO RESULT")
+
+if __name__ == "__main__":
+    capture("TESTIMAGE1")
