4b92b8a39e16384d495fa4f02367195af9ce1f33
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-16 13:58:06 -0800

    Update videoserver.py

--- raspberry_pi/information_system/test/videoserver.py
+++ raspberry_pi/information_system/test/videoserver.py
@@ -1,10 +1,17 @@
 import socketio
 import base64
+from flask_socketio import emit, SocketIO
+from flask import Flask
+
+app = Flask(__name__)
 
 
-sio = socketio.Server()
+sio = SocketIO(app)
 
 
 @sio.on('data')
 def handle_image(data_image):
     print('received image')
+
+if __name__ == "__main__":
+    app.run()
