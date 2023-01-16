38ce31d0391671d08feaae124a11c180e832e629
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-15 22:12:24 -0800

    Update video_capture.py

--- raspberry_pi/information_system/test/video_capture.py
+++ raspberry_pi/information_system/test/video_capture.py
@@ -1,21 +1,15 @@
 import numpy as np
-import time
-import matplotlib.pyplot as plt
 print(np.__version__)
 
 import cv2
 print(cv2.__version__)
 
-#CV2.imwrite says it does not have permission
-def imwrite(name, img):
-    plt.imshow(img)
-    plt.savefig(name)
 cam = cv2.VideoCapture(0)
 
 result, img = cam.read()
 
 if result:
-    imwrite("/home/tritonrobotics2/Desktop/TESTIMAGE1.jpg", img)
+    cv2.imwrite("/home/tritonrobotics2/Desktop/TESTIMAGE1.jpg", img)
     print("saved image")
 
 else:
