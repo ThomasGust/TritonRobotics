718653ceb3711b64b2cb27e89da53330df175389
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-15 22:02:08 -0800

    Update video_capture.py

--- raspberry_pi/information_system/test/video_capture.py
+++ raspberry_pi/information_system/test/video_capture.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 print(np.__version__)
 
 import cv2
@@ -11,6 +12,8 @@ result, img = cam.read()
 
 if result:
     cv2.imshow("test", img)
+    cv2.show()
+    time.sleep(10)
     print("saved image")
 
 else:
