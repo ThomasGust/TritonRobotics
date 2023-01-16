7beec3a544cfc5f48672ddf884469e5b8c83b078
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-15 21:55:31 -0800

    Update video_capture.py

--- raspberry_pi/information_system/test/video_capture.py
+++ raspberry_pi/information_system/test/video_capture.py
@@ -1,4 +1,8 @@
+import numpy as np
+print(np.__version__)
+
 import cv2
+print(cv2.__version__)
 
 
 cam = cv2.VideoCapture(0)
@@ -6,7 +10,7 @@ cam = cv2.VideoCapture(0)
 result, img = cam.read()
 
 if result:
-    cv2.imwrite("IMG.png", img)
+    cv2.imwrite("Desktop\\IMG.png", img)
     print("saved image")
 
 else:
