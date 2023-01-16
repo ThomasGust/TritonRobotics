913815591b9dee3adff072049d7cf13eccbb6fbd
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-16 08:47:12 -0800

    Functionalize start_server and start_client

--- raspberry_pi/information_system/server/server.py
+++ raspberry_pi/information_system/server/server.py
@@ -1,20 +1,26 @@
 import socket
 
-IP = 'PUT SERVER IP (RASPI) HERE'
-PORT = 5005
-BS = 1024
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((IP, PORT))
-sock.listen(1)
+def start_server(IP):
+    #IP = 'PUT SERVER IP (RASPI) HERE'
+    PORT = 5005
+    BS = 1024
 
-connection, address = sock.accept()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((IP, PORT))
+    sock.listen(1)
 
-print(f"ADDR: {address}")
+    connection, address = sock.accept()
 
-while 1:
-    data = connection.recv(BS)
-    if not data: break
-    print(f"received: {data}")
-    connection.send(data)
-connection.close()
+    print(f"ADDR: {address}")
+
+    while 1:
+        data = connection.recv(BS)
+        if not data: break
+        print(f"received: {data}")
+        connection.send(data)
+    connection.close()
+
+if __name__ == "__main__":
+    ip_addr = input("Please put server ip address here: ")
+    start_server(ip_addr)
