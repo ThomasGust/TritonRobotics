19f006c73afe012840cf0004775382b9d8bd21f3
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-17 18:41:41 -0800

    Use full bluerobotics webcam resolution

--- raspberry_pi/information_system/test/video_capture.py
+++ raspberry_pi/information_system/test/video_capture.py
@@ -7,6 +7,13 @@ print(cv2.__version__)
 
 cam = cv2.VideoCapture(0)
 
+codec = 0x47504A4D  # MJPG
+cam.set(cv2.CAP_PROP_FPS, 30.0)
+cam.set(cv2.CAP_PROP_FOURCC, codec)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+
 def capture(name):
     result, img = cam.read()
 
