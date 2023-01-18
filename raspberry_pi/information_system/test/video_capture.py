afd1320d306a3883b10a7fcfa3988cfdb968b724
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-17 19:03:14 -0800

    Update video_capture.py

--- raspberry_pi/information_system/test/video_capture.py
+++ raspberry_pi/information_system/test/video_capture.py
@@ -14,10 +14,13 @@ def capture(name):
     result, img = cam.read()
 
     if result:
-        cv2.imwrite(f"/home/tritonrobotics2/Desktop/{name}.jpg", img)
+        #cv2.imwrite(f"/home/tritonrobotics2/Desktop/{name}.jpg", img)
         retval, buffer = cv2.imencode('.jpg', img)
         pg_as_text = base64.b64encode(buffer)
-        print(pg_as_text)
+        decoded = base64.b64decode(pg_as_text)
+
+        with open(f'{name}.jpg', "wb") as f:
+            f.write(decoded)
         print("saved image")
 
     else:
