5ca0b3348e287ea57324db5d72f93c6a3e73c660
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-21 12:54:31 -0800

    UPDATE STUFF

--- raspberry_pi/information_system/test/image_zmq_hub.py
+++ raspberry_pi/information_system/test/image_zmq_hub.py
@@ -1,6 +1,11 @@
 import imagezmq
+import cv2
 
 image_hub = imagezmq.ImageHub()
 
 sender_name, image = image_hub.recv_image()
 image_hub.send_reply(b'OK')
+print("RECEIVED IMAGE")
+
+cv2.imshow("THIS IS A TEST", image)
+cv2.waitKey(0)
--- raspberry_pi/information_system/test/image_zmq_sender.py
+++ raspberry_pi/information_system/test/image_zmq_sender.py
@@ -1,9 +1,11 @@
 import socket
 import imagezmq
+import cv2
 
-sender = imagezmq.ImageSender(connect_to='tcp://169.254.82.153:5555')
+sender = imagezmq.ImageSender(connect_to='tcp://169.254.222.33:5555')
 
 sender_name = socket.gethostname() # send your hostname with each image
 
-image = open("test1.jpg",'rb')
-sender.send_image(sender_name, image)
+image = cv2.imread("test1.png")
+print(type(image))
+sender.send_image_pubsub(sender_name, image)
