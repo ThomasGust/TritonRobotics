4a8ac9f1547f44f85add178ca0ee67dea28811ae
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-21 13:02:06 -0800

    EDIT ZMQ MY COMMIT DESCRIPTION MUCH GOOD

--- raspberry_pi/information_system/test/image_zmq_hub.py
+++ raspberry_pi/information_system/test/image_zmq_hub.py
@@ -3,10 +3,12 @@ import cv2
 
 image_hub = imagezmq.ImageHub()
 print("INITIALIZED IAMGE HUB")
-sender_name, image = image_hub.recv_image()
-print("RECEIVED IMAGE")
-image_hub.send_reply(b'OK')
-print("RECEIVED IMAGE")
 
-cv2.imshow("THIS IS A TEST", image)
-cv2.waitKey(0)
+while True:
+    sender_name, image = image_hub.recv_image()
+    print("RECEIVED IMAGE")
+    image_hub.send_reply(b'OK')
+    print("RECEIVED IMAGE")
+
+    cv2.imshow("THIS IS A TEST", image)
+    cv2.waitKey(1)
--- raspberry_pi/information_system/test/image_zmq_sender.py
+++ raspberry_pi/information_system/test/image_zmq_sender.py
@@ -1,11 +1,14 @@
 import socket
 import imagezmq
 import cv2
+from imutils.video import VideoStream
 
 sender = imagezmq.ImageSender(connect_to='tcp://169.254.222.33:5555')
 
+webcam = VideoStream().start()
 sender_name = socket.gethostname() # send your hostname with each image
 
-image = cv2.imread("test1.png")
-print(type(image))
-sender.send_image_pubsub(sender_name, image)
+while True:
+    image = webcam.read()
+    print(type(image))
+    sender.send_image_pubsub(sender_name, image)
