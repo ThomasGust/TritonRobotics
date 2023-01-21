27ae123927a9b5ebc79686962167cd90a410f1e6
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-21 12:40:20 -0800

    TESTING

--- raspberry_pi/information_system/test/image_zmq_hub.py
+++ raspberry_pi/information_system/test/image_zmq_hub.py
@@ -3,8 +3,8 @@ import imagezmq
 hub = imagezmq.ImageHub()
 print("INITIALIZED HUB")
 while True:
-    print("RECEIVED IMAGE")
     name, frame = hub.recv_image()
+    print("RECEIVED IMAGE")
     cv2.imshow(name, frame)
     cv2.waitKey(1)
     hub.send_reply(b'OK')
