960d4929f5a2498413f5d22b60f9b7114918148d
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-21 12:55:36 -0800

    Update image_zmq_hub.py

--- raspberry_pi/information_system/test/image_zmq_hub.py
+++ raspberry_pi/information_system/test/image_zmq_hub.py
@@ -2,8 +2,9 @@ import imagezmq
 import cv2
 
 image_hub = imagezmq.ImageHub()
-
+print("INITIALIZED IAMGE HUB")
 sender_name, image = image_hub.recv_image()
+print("RECEIVED IMAGE")
 image_hub.send_reply(b'OK')
 print("RECEIVED IMAGE")
 
