88ddbc6086e460cc73900ee32a4114dc1ce37863
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-21 12:31:17 -0800

    Update image_zmq_sender.py

--- raspberry_pi/information_system/test/image_zmq_sender.py
+++ raspberry_pi/information_system/test/image_zmq_sender.py
@@ -7,7 +7,7 @@ sender = imagezmq.ImageSender(connect_to="tcp://169.254.82.153:5005")
 
 name = socket.gethostname()
 
-picam = VideoStream(usePiCamera=True).start()
+picam = VideoStream().start()
 time.sleep(2.0)
 while True:
     img = picam.read()
