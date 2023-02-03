f206e433b55e3248dba48c9e21ce9bd95534418b
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-02-03 15:46:12 -0800

    Add ImageSender Code to rov.py

--- raspberry_pi/information_system/rov/rov.py
+++ raspberry_pi/information_system/rov/rov.py
@@ -1,6 +1,35 @@
+from threading import Thread
+from imutils.video import VideoStream
+import imagezmq
 
+class Motor:
+
+    def __init__(self):
+        pass
+
+class MotorController:
+
+    def __init__(self):
+        pass
+
+
+class ImageSender(Thread):
+    
+    def __init__(self, name):
+        Thread.__init__(self)
+        self.webcam = VideoStream().start()
+        self.name = name
+        
+    def run(self, client_ip, client_vid_port):
+        sender = imagezmq.ImageSender(connect_to=f"{client_ip}:{client_vid_port}")
+        
+        on = True
+
+        while on:
+            img = self.webcam.read()
+            sender.send_image_reqrep(self.name, img)
 
 class Rov:
 
     def __init__(self):
-        pass
+        self.image_sender = ImageSender(name="MainROV")
--- raspberry_pi/information_system/rov/top.py
+++ raspberry_pi/information_system/rov/top.py
@@ -1,4 +1,4 @@
-
+import imagezmq
 
 class Top:
 
